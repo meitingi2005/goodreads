95c43349adb038c4e37b9014f01d78c8d0d71972
raveenas1025 <raveenas1025@hstat.org>
2017-03-07 18:44:57 +0000

    learn inspect

--- models/model.rb
+++ models/model.rb
@@ -3,4 +3,7 @@
 require 'goodreads'
 require 'pp'
 client = Goodreads::Client.new(api_key: "apl03Pv1Wh1qnhUF6iuwQ", api_secret: "m7bx1HSNHdMD36wUP7jhIyucmgt1gGpAnQJGkh7erY")
-puts pp client.book_by_title("The Great Gatsby")
+
+#puts displays string with quotes, pp does not
+puts client.book_by_title("The Great Gatsby").authors.author.name
+puts client.book_by_title("The Great Gatsby").description # use regex to rid of tags

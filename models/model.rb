62d3151d4e78dba58e2cf25d82e7dc193b38805f
meaganc2976 <meaganc2976@hstat.org>
2017-03-07 02:34:09 +0000

    added a result.erb, created inputs for the index, and added the route to result.erb in app controller

--- models/model.rb
+++ models/model.rb
@@ -3,4 +3,7 @@
 require 'goodreads'
 require 'pp'
 client = Goodreads::Client.new(api_key: "apl03Pv1Wh1qnhUF6iuwQ", api_secret: "m7bx1HSNHdMD36wUP7jhIyucmgt1gGpAnQJGkh7erY")
-puts pp client.book_by_title("The Great Gatsby")
+puts client.book_by_title("Catcher in the Rye").authors.author.name #argument will become variable for user input
+puts client.book_by_title("Catcher in the Rye").description #use regex to find tags and delete them so we can pust the quote
+
+# user_book_author = client.book_by_title(user_book_title).authors.author.name 

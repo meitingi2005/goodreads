42f6fae8203e6b93d002616290b89930d56ec0ae
meitingi2005 <meitingi2005@hstat.org>
2017-03-07 18:42:43 +0000

    author

--- models/model.rb
+++ models/model.rb
@@ -3,4 +3,8 @@
 require 'goodreads'
 require 'pp'
 client = Goodreads::Client.new(api_key: "apl03Pv1Wh1qnhUF6iuwQ", api_secret: "m7bx1HSNHdMD36wUP7jhIyucmgt1gGpAnQJGkh7erY")
-puts pp client.book_by_title("The Great Gatsby")
+# pp client.book_by_title("The Great Gatsby").authors.author.name
+# pp client.book_by_title("The Great Gatsby").description
+# pp client.book_by_title("The Great Gatsby").popular_shelves.shelf[3]
+author = client.author("3190")
+puts author.about

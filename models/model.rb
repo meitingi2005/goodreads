341fc7bb32ca770de4aec0196a7dbc5b18077581
meitingi2005 <meitingi2005@hstat.org>
2017-03-08 05:23:20 +0000

    pull

--- models/model.rb
+++ models/model.rb
@@ -1,68 +1,10 @@
-require 'goodreads'
+# key: apl03Pv1Wh1qnhUF6iuwQ
+# secret: m7bx1HSNHdMD36wUP7jhIyucmgt1gGpAnQJGkh7erY
+require 'goodreads' 
 require 'pp'
-
-
-
-class Book
-    attr_reader :book_title, :book_description, :book_img, :book_author
-    def initialize(book_title)
-        @book_title = book_title
-        @client = Goodreads::Client.new(api_key: "apl03Pv1Wh1qnhUF6iuwQ", api_secret: "m7bx1HSNHdMD36wUP7jhIyucmgt1gGpAnQJGkh7erY")
-        # @user_book_author = user_book_author
-        # @user_book_description = user_book_description
-        # @user_book_img = user_book_img
-    end
-    def get_title
-        @book_title = @client.book_by_title(@book_title).title
-    end
-    def get_author
-        @book_author = @client.book_by_title(@book_title).authors.author.name 
-    end
-    def get_description
-        @book_description = @client.book_by_title(@book_title).description
-    end
-    # def get_image
-    #      book_image = @client.book_by_title(@book_title).
-    # end
-end
-
-book_title = Book.new("Catcher in the Rye")
-puts book_title.get_description
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-pp client.book_by_title("Catcher in the Rye").buy_links.buy_link.first.name
-pp client.book_by_title("Catcher in the Rye").buy_links.buy_link.first.link
-
-# pp client.book_by_title("Catcher in the Rye").similar_books.book[1].authors.author.name
-
+client = Goodreads::Client.new(api_key: "apl03Pv1Wh1qnhUF6iuwQ", api_secret: "m7bx1HSNHdMD36wUP7jhIyucmgt1gGpAnQJGkh7erY")
+# pp client.book_by_title("The Great Gatsby").authors.author.name
+# pp client.book_by_title("The Great Gatsby").description
+# pp client.book_by_title("The Great Gatsby").popular_shelves.shelf[3]
+author = client.author("3190")
+puts author.about

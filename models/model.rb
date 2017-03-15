68bca0a3eb0e65e385b67500cdc29f068bdd5418
raveenas1025 <raveenas1025@hstat.org>
2017-03-15 18:39:29 +0000

    merge

--- models/model.rb
+++ models/model.rb
@@ -1,46 +1,51 @@
-require 'goodreads' 
+require 'goodreads'  
 require 'pp'
+
+
 class Book
-
-
-    attr_reader :book_title, :book_description, :book_img, :book_author, :similar_book_titles, :similar_book_authors, :similar_book_description, :book_pages, :similar_books, :other_info, :similar_book_description, :error_message, :book_location_name
-
-    
+    attr_reader :book_title, :book_description, :book_img, :book_author, :book_pages, :book_location_name_link, :similar_books, :other_info, :similar_book_description, :error_message, :book_rating, :book_url
     def initialize(book_title,other_info)
         @book_title = book_title
         @books = []
         @similar_books = []
         @other_info = other_info
-        @book_pages = []
+         @book_pages = []
         @client = Goodreads::Client.new(api_key: "apl03Pv1Wh1qnhUF6iuwQ", api_secret: "m7bx1HSNHdMD36wUP7jhIyucmgt1gGpAnQJGkh7erY")
     end
     
     def get_title
-        begin 
+        # begin 
             @book_title = @client.book_by_title(@book_title).title
-        rescue
-            flash[:error] = "Your book was not found"
-            render 'index.erb'
-        end
+        # rescue
+        #     flash[:error] = "Your book was not found"
+        #     render 'index.erb'
+        # end
         
     end
     
+
+    
     def get_author
+        begin
         @book_author = @client.book_by_title(@book_title).authors.author.name
+        rescue
+        @book_title = @client.book_by_title(@book_title).authors.author[0]["name"]
+        end
     end
 
     def get_image
         @book_image = @client.book_by_title(@book_title).image_url
+
     end
 
-    def get_goodreads_url
-        # @book_location_name_link = []
-        # @book_location_name_link << 
-        @book_location_name = @client.book_by_title(@book_title).url
-        # @book_location_name_link << @client.book_by_title(@book_title).buy_links.buy_link.first.link
+    def get_location
+        @book_location_name_link = []
+        @book_location_name_link << @client.book_by_title(@book_title).buy_links.buy_link.first.name
+        @book_location_name_link << @client.book_by_title(@book_title).buy_links.buy_link.first.link
     end
     
-
+    
+    
     def get_similar_books
         # @similar_books = []
         for i in 0..2
@@ -78,7 +83,14 @@ class Book
         "This may take more than two weeks to read."
         end
     end
-
+    
+    def get_rating 
+        @book_rating= @client.book_by_title(@book_title).average_rating
+    end
+    
+    def get_goodreads_url
+         @book_url= @client.book_by_title(@book_title).url
+    end
 
 end
 
@@ -91,12 +103,8 @@ end
 # catcher = Book.new("ThhDQV",{})
 # puts catcher.get_title
 
-# catcher = Book.new("The Grapes of Wrath",{})
-# puts catcher.get_location
+# catcher = Book.new("Emma",{})
+# puts catcher.get_title
 
 # puts catcher.similar_book_title[0]
 # puts catcher.similar_book_description
-
-
-
-# .work.link
